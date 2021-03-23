76dc9a3b4f6c9efafb1ee6432da63c5849c48819
LintaRahman <zcablra@ucl.ac.uk>
2021-03-23 11:18:18 +0000

    updated switch frames

--- pyQt.py
+++ pyQt.py
@@ -7,40 +7,43 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QColor, QIcon, QPixmap, QFont
 from PyQt5.QtWidgets import QFileDialog, QLabel, QVBoxLayout
 #from TruthTableGenerator import TruthTableFiller
+import TruthTableGenerator
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
-        self.setGeometry(50, 50, 1200, 700)
-        self.setWindowTitle("Truth Table Filler")
+        self.setGeometry(50, 50, 1000, 600)
+        self.setWindowTitle("Welcome to Truth Tables don't lie")
         backGround = self.palette()
-        backGround.setColor(self.backgroundRole(), QColor(15, 102, 102))
+        backGround.setColor(self.backgroundRole(), QColor(15,102,102))
         self.setPalette(backGround)
         self.widgets()
 
     def widgets(self):
         title = QLabel("Welcome to Truth Tables don't lie", self)
-        title.move(300, 30)
+        title.move(200, 30)
         title.setFont(QFont('Times', 20))
         title.setStyleSheet("color: white;")
         title.adjustSize()
 
-        TTOButton = QtWidgets.QPushButton(self)
-        TTOButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        # TTOButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        TTOButton.resize(300, 300)
-        TTOButton.move(100, 100)
-        TTOButton.setStyleSheet("background-image: url('icon.png'); border: none;")
-        exButton = QtWidgets.QPushButton("Create Exercise", self)
-        # button.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        exButton.resize(300, 300)
-        exButton.move(450, 100)
-        exButton.setStyleSheet("background-color: white;")
+        self.TTOButton = QtWidgets.QPushButton(self)
+        self.TTOButton.clicked.connect(self.showTTF)
+        self.TTOButton.resize(300, 300)
+        self.TTOButton.move(100, 100)
+        self.TTOButton.setToolTip("<h3>Start the Session</h3>")
+        self.TTOButton.setStyleSheet("background-image: url('icon.png'); border: none;")
+        # exButton = QtWidgets.QPushButton("Create Exercise", self)
+        # # button.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        # exButton.resize(300, 300)
+        # exButton.move(450, 100)
+        # exButton.setStyleSheet("background-color: white;")
 
-        fileChooser = QtWidgets.QPushButton("QFileDialog object", self)
+        fileChooser = QtWidgets.QPushButton(self)
         fileChooser.clicked.connect(self.getfile)
         fileChooser.resize(300, 300)
-        fileChooser.move(800, 100)
+        fileChooser.move(600, 100)
+        fileChooser.setToolTip("<h3>Create Exercise</h3>")
+        fileChooser.setStyleSheet("background-image: url('createexercise.png'); border: none;")
 
         self.show()
 
@@ -51,8 +54,13 @@ class Window(QtWidgets.QMainWindow):
 
     def getfile(self):
         fname = QFileDialog.getOpenFileName(self, 'Open file',
-                                            'c:\\')
+                                            'c:\\', "Text files (*.txt)")
         print(fname)
+
+    def showTTF(self):
+        self.TTF = TruthTableGenerator.TTFWindow()
+        self.TTF.show()
+        self.hide()
 
     # def getfiles(self):
     #     dialogue = QFileDialog()
@@ -76,6 +84,5 @@ def run():
     GUI = Window()
     GUI.show()
     sys.exit(app.exec_())
-
 
 run()
